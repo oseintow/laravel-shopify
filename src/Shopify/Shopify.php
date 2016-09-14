df5b50c48bfe2562c09ece1f3426f61307849d58
michael ntow <oseintow@gmail.com>
2016-09-14 17:53:17 +0000

    Testing status code

--- src/Shopify/Shopify.php
+++ src/Shopify/Shopify.php
@@ -107,6 +107,14 @@ class Shopify{
                 'timeout'  => 60.0
             ]);
 
+        \Log::info($response->getStatusCode());
+        \Log::info($response);
+
+//        if (isset($response['errors']) or ($this->last_response_headers['http_status_code'] >= 400)){
+//            //if (isset($response['errors'])) Log::error($response['errors']);
+//            throw new ShopifyApiException($method, $path, $params, $this->last_response_headers, $response);
+//        }
+
         $this->parseHeaders($response);
 
         return $response;
